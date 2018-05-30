a515a6a25146583a9da4d23405050a6dd08ea237
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2018-05-31 01:45:58 +0300

    write test for successful user registration

---
+++ test_auth.py
@@ -0,0 +1,21 @@
+from flask_testing import TestCase
+from run import app
+import json
+
+
+class UserAuthTest(TestCase):
+
+    def create_app(self):
+        return app
+
+    def test_register(self):
+        """ Test for successful user login """
+        with self.client:
+            response = self.client.post(
+                "/user/register",
+                content_type='application/json',
+                data=json.dumps(dict(name="Gideon B", email="email@email.com", password="secret"))
+                )            
+            reply = json.loads(response.data)
+            self.assertEquals(reply["message"], "successfully registered")
+            self.assertEquals(response.status_code, 201)
