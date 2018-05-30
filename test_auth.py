ac9270f05aeb244317ace616ed57bb41b40ac2e2
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2018-05-31 02:27:37 +0300

    write test for login endpoint

--- test_auth.py
+++ test_auth.py
@@ -55,3 +55,23 @@ class UserAuthTest(TestCase):
             reply = json.loads(response.data)
             self.assertEquals(reply["message"], "Missing email parameter")
             self.assertEquals(response.status_code, 400)
+
+    def test_user_login_successfully(self):
+        """ Test for login end point """
+        with self.client:
+            res = self.client.post(
+                "1/user/register",
+                content_type='application/json',
+                data=json.dumps(dict(username="Gideon B", email="email@email.com", password="secret"))
+                )            
+            # reply = json.loads(res.data)
+
+            response = self.client.post(
+                "1/user/login",
+                content_type='application/json',
+                data=json.dumps(dict(email="email@email.com", password="secret"))
+                )            
+            reply = json.loads(response.data)
+
+            self.assertEquals(reply["message"], "sucessfully logged in")
+            self.assertEquals(response.status_code, 200)
