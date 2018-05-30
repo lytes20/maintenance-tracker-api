e22259f8b968e3474e0b68a535cb3d5e29093bd8
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2018-05-31 02:20:32 +0300

    write tests to cover validation

--- test_auth.py
+++ test_auth.py
@@ -9,7 +9,7 @@ class UserAuthTest(TestCase):
         return app
 
     def test_register(self):
-        """ Test for successful user login """
+        """ Test for successful user register """
         with self.client:
             response = self.client.post(
                 "1/user/register",
@@ -19,3 +19,39 @@ class UserAuthTest(TestCase):
             reply = json.loads(response.data)
             self.assertEquals(reply["message"], "successfully registered")
             self.assertEquals(response.status_code, 201)
+
+    def test_register_with_empty_password(self):
+        """ Test for empty password validation """
+        with self.client:
+            response = self.client.post(
+                "1/user/register",
+                content_type='application/json',
+                data=json.dumps(dict(username="Gideon B", email="email@email.com", password=""))
+                )            
+            reply = json.loads(response.data)
+            self.assertEquals(reply["message"], "Missing password parameter")
+            self.assertEquals(response.status_code, 400)
+
+    def test_register_with_empty_username(self):
+        """ Test for empty username validation """
+        with self.client:
+            response = self.client.post(
+                "1/user/register",
+                content_type='application/json',
+                data=json.dumps(dict(username="", email="email@email.com", password="secret"))
+                )            
+            reply = json.loads(response.data)
+            self.assertEquals(reply["message"], "Missing username parameter")
+            self.assertEquals(response.status_code, 400)
+
+    def test_register_with_empty_email(self):
+        """ Test for empty email validation """
+        with self.client:
+            response = self.client.post(
+                "1/user/register",
+                content_type='application/json',
+                data=json.dumps(dict(username="Gideon B", email="", password="secret"))
+                )            
+            reply = json.loads(response.data)
+            self.assertEquals(reply["message"], "Missing email parameter")
+            self.assertEquals(response.status_code, 400)
